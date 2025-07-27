fa7551a5496c87aca12544e9f284176041241354
vitorsillvaz <vitorsillvaemail@gmail.com>
2025-07-27 13:21:12 -0300

    Finalizando a etapa 1

--- app/jobs/Inicializador.java
+++ app/jobs/Inicializador.java
@@ -12,12 +12,24 @@ public class Inicializador extends Job{
 	@Override
 	public void doJob() throws Exception {
 		if (Categoria.count() == 0) {
-			Categoria proteina = new Categoria("Proteina");
+			Categoria proteina = new Categoria("Proteína");
 			proteina.save();
 			
+			Categoria creatina = new Categoria("Creatina");
+			creatina.save();
+			
+			Categoria vitamina = new Categoria("Vitaminas");
+			vitamina.save();
+			
+			Categoria pretreino = new Categoria("Pré-treino");
+			pretreino.save();
+			
+			Categoria postreino = new Categoria("Pós-treino");
+			postreino.save();
+			
 			Produto p1 = new Produto();
-			p1.nomeProduto = "João da Silva";
-			p1.preco = 153.0;
+			p1.nomeProduto = "Whey Protein Concentrado";
+			p1.preco = 124.90;
 			p1.categoria = proteina;
 			p1.save();
 		}
