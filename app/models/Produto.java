2c9b82b25fdcbb97cb9d07dee6e2d310abcdf9cb
vitorsillvaz <vitorsillvaemail@gmail.com>
2025-10-16 14:41:42 -0300

    upload

--- app/models/Produto.java
+++ app/models/Produto.java
@@ -1,39 +1,39 @@
 package models;
 
-import java.util.Date;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import play.data.validation.Required;
+import play.db.jpa.Blob;
 import play.db.jpa.Model;
 
 @Entity
 public class Produto extends Model {
-	@Required
-	public String nomeProduto;
-	
-	@Required
-	public double preco;
-	
-	@ManyToOne
-	public Categoria categoria;
-	
-	@Enumerated(EnumType.STRING)
-	public Status status;
-	
-	public Produto() {
-		this.status = Status.ATIVO;
-	}
-	public Produto(String nomeProduto, double preco) {
-		this.nomeProduto = nomeProduto;
-		this.preco = preco;
-	}
 
+    @Required
+    public String nomeProduto;
+
+    @Required
+    public double preco;
+
+    @ManyToOne
+    public Categoria categoria;
+
+    @Enumerated(EnumType.STRING)
+    public Status status;
+
+    public Blob foto;
+    public String photofileName;
+
+    public Produto() {
+        this.status = Status.ATIVO;
+    }
+
+    public Produto(String nomeProduto, double preco) {
+        this.nomeProduto = nomeProduto;
+        this.preco = preco;
+        this.status = Status.ATIVO;
+    }
 }
